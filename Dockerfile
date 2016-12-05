dade7c3846a8f7433151703254d88dc4d0e49ef7
chentongming <chentongming@miyabaobei.com>
2016-12-05 09:38:43 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM php:5.6.28-fpm-alpine
+RUN apk update
---
+++ bin/default.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+certbot certonly --webroot -w /tmp/ -d justdofun.com -d www.justdofun.com -d intra.justdofun.com -d api.justdofun.com -t
